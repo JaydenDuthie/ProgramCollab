daaceda5e83ae8f9f720bf05996916efbb09235c
Jayden Duthie (1000142765) <DUTHJR1@student.op.ac.nz>
2026-09-22 11:10:18 +1200

    Added a }

--- TestProg.cs
+++ TestProg.cs
@@ -59,43 +59,44 @@ namespace Test
         public static void Alex()
         {
             {
-            Random rand = new Random();
-            char user, comp;
-            int number;
+                Random rand = new Random();
+                char user, comp;
+                int number;
 
-            Console.WriteLine("Please pick either R, P or S: ");
-            user = Convert.ToChar(Console.ReadLine());
-            number = rand.Next(3);
-            switch (number)
-            {
-                case 0:
-                    comp = 'R';
-                    break;
-                case 1:
-                    comp = 'P';
-                    break;
-                default:
-                    comp = 'S';
-                    break;
-            }
-            Console.WriteLine($"The computer has chosen {comp}");
-            if (((user == 'R') && (comp == 'S')) || ((user == 'S') && (comp == 'p')) || ((user == 'P') && (comp == 'R')))
-            {
-                Console.WriteLine("User wins");
-            }
-            else
-            {
-                if (user == comp)
+                Console.WriteLine("Please pick either R, P or S: ");
+                user = Convert.ToChar(Console.ReadLine());
+                number = rand.Next(3);
+                switch (number)
                 {
-                    Console.WriteLine("Draw game");
+                    case 0:
+                        comp = 'R';
+                        break;
+                    case 1:
+                        comp = 'P';
+                        break;
+                    default:
+                        comp = 'S';
+                        break;
+                }
+                Console.WriteLine($"The computer has chosen {comp}");
+                if (((user == 'R') && (comp == 'S')) || ((user == 'S') && (comp == 'p')) || ((user == 'P') && (comp == 'R')))
+                {
+                    Console.WriteLine("User wins");
                 }
                 else
                 {
-                    Console.WriteLine("Computer wins");
+                    if (user == comp)
+                    {
+                        Console.WriteLine("Draw game");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Computer wins");
+                    }
                 }
+                Console.WriteLine("Press ENTER key to close");
+                Console.ReadLine();
             }
-            Console.WriteLine("Press ENTER key to close");
-            Console.ReadLine();
         }
 
         public static void Seth()
