543011f599a688dc5207fdaf73b32fc46e6bc777
Alexander Crosado (1000050232) <CROSAD1@student.op.ac.nz>
2026-09-22 10:58:15 +1200

    Alex code section

--- TestProg.cs
+++ TestProg.cs
@@ -1,10 +1,48 @@
-﻿namespace Test
+﻿using System;
+
+namespace Test
 {
     internal class TestProg
     {
-        static void Main(string[] args)
+        private void Alex()
         {
-            Console.WriteLine("Hello, World!");
+            Random rand = new Random();
+            char user, comp;
+            int number;
+
+            Console.WriteLine("Please pick either R, P or S: ");
+            user = Convert.ToChar(Console.ReadLine());
+            number = rand.Next(3);
+            switch (number)
+            {
+                case 0:
+                    comp = 'R';
+                    break;
+                case 1:
+                    comp = 'P';
+                    break;
+                default:
+                    comp = 'S';
+                    break;
+            }
+            Console.WriteLine($"The computer has chosen {comp}");
+            if (((user == 'R') && (comp == 'S')) || ((user == 'S') && (comp == 'p')) || ((user == 'P') && (comp == 'R')))
+            {
+                Console.WriteLine("User wins");
+            }
+            else
+            {
+                if (user == comp)
+                {
+                    Console.WriteLine("Draw game");
+                }
+                else
+                {
+                    Console.WriteLine("Computer wins");
+                }
+            }
+            Console.WriteLine("Press ENTER key to close");
+            Console.ReadLine();
         }
     }
 }
